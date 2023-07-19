e25843c05c864ebd09d00a341de165fb219e2254
xiaotai <2703093894@qq.com>
2023-07-19 10:28:46 +0800

    first commit

---
+++ src/test/java/life/family/community/CommunityApplicationTests.java
@@ -0,0 +1,14 @@
+package life.family.community;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CommunityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
