37eac27f4baf09b2c56b3d8dc09288d2d0d675cf
xiaotai <2703093894@qq.com>
2023-07-20 19:33:01 +0800

    add mysql

--- src/main/java/life/family/community/controller/AuthorizeController.java
+++ src/main/java/life/family/community/controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package life.family.community.controller;
 
 import life.family.community.dto.AccessTokenDTO;
 import life.family.community.dto.GithubUser;
+import life.family.community.mapper.UserMapper;
+import life.family.community.model.User;
 import life.family.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -16,13 +21,19 @@ public class AuthorizeController {
 
     @Value("${github.client.id}" )
     private String clientId;
+
     @Value("${github.Client.secret}")
     private String clientSecret;
+
     @Value("${github.Redirect.uri}")
     private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
-                           @RequestParam(name = "state")String state){
+                           @RequestParam(name = "state")String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirectUri);
@@ -30,8 +41,22 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        //System.out.println(githubUser.getName());
+        if(githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登陆成功，写cookie和session
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        }else{
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
---
+++ src/main/java/life/family/community/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package life.family.community.mapper;
+
+import life.family.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name, account_id, token, gmt_create, gmt_modified) values (#{name}, #{accountId}, #{token}, #{gmtCreate}, #{gmtModified})")
+    void insert(User user);
+
+}
