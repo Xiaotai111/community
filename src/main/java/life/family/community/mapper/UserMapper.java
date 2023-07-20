d53956230aa8be693690e3bda8d1fbd1b390ad78
xiaotai <2703093894@qq.com>
2023-07-20 20:12:33 +0800

    add check cookies

--- src/main/java/life/family/community/mapper/UserMapper.java
+++ src/main/java/life/family/community/mapper/UserMapper.java
@@ -3,7 +3,8 @@ package life.family.community.mapper;
 import life.family.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 
 @Mapper
@@ -11,4 +12,6 @@ public interface UserMapper {
     @Insert("insert into user (name, account_id, token, gmt_create, gmt_modified) values (#{name}, #{accountId}, #{token}, #{gmtCreate}, #{gmtModified})")
     void insert(User user);
 
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
