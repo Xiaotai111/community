8feec1feaf8b4c4eba1f0ca09087db5a2436557d
xiaotai <2703093894@qq.com>
2023-07-21 19:02:44 +0800

    发布问题，显示问题

--- src/main/java/life/family/community/controller/IndexController.java
+++ src/main/java/life/family/community/controller/IndexController.java
@@ -1,35 +1,48 @@
 package life.family.community.controller;
 
+import life.family.community.dto.QuestionDTO;
+import life.family.community.mapper.QuestionMapper;
 import life.family.community.mapper.UserMapper;
+import life.family.community.model.Question;
 import life.family.community.model.User;
+import life.family.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
     @Autowired
     private UserMapper userMapper;
-    @GetMapping("/")
-    public String index(HttpServletRequest request){
 
+    @Autowired
+    private QuestionService questionService;
+    @GetMapping("/")
+    public String index(HttpServletRequest request,
+                Model model){
         Cookie[] cookies = request.getCookies();
-        for(Cookie cookie:cookies){
-            if(cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user = userMapper.findByToken(token);
-                if(user != null){
-                    request.getSession().setAttribute("user", user);
+        if (cookies != null && cookies.length != 0) { // 进行空值检查
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    if (token != null && !token.isEmpty()) { // 再次进行空值检查
+                        User user = userMapper.findByToken(token);
+                        if (user != null) {
+                            request.getSession().setAttribute("user", user);
+                        }
+                    }
+                    break;
                 }
-                break;
             }
         }
-
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions", questionList);
         return "index";
     }
 }
--- src/main/java/life/family/community/controller/PublishController.java
+++ src/main/java/life/family/community/controller/PublishController.java
@@ -1,12 +1,82 @@
 package life.family.community.controller;
 
+import life.family.community.mapper.QuestionMapper;
+import life.family.community.mapper.UserMapper;
+import life.family.community.model.Question;
+import life.family.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/publish")
     public String publish(){
         return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam(value = "title", required = false)String title,
+            @RequestParam(value = "description", required = false)String description,
+            @RequestParam(value = "tag", required = false)String tag,
+            HttpServletRequest request,
+            Model model){
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
+        if(title == null || "".equals(title)){
+            model.addAttribute("error", "标题不能为空");
+            return "publish";
+        }
+        if(description == null || "".equals(description)){
+            model.addAttribute("error", "问题补充不能为空");
+            return "publish";
+        }
+        if(tag == null || "".equals(tag)){
+            model.addAttribute("error", "标签不能为空");
+            return "publish";
+        }
+        Cookie[] cookies = request.getCookies();
+        User user = null;
+        if (cookies != null) { // 进行空值检查
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    if (token != null && !token.isEmpty()) { // 再次进行空值检查
+                        user = userMapper.findByToken(token);
+                        if (user != null) {
+                            request.getSession().setAttribute("user", user);
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
+            return "publish";
+        }
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
+        questionMapper.create(question);
+        return "redirect:/";
     }
 }
